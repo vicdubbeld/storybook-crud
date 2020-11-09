f2a4de8714155d1ab34c89f59c96df008df5517f
vicdubbeld <vicdubbeld@gmail.com>
2020-11-09 15:30:47 -0800

    Handlebars...engaged

--- storybooks/app.js
+++ storybooks/app.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
+const exphbs = require("express-handlebars");
 const connectDB = require("./config/db");
+
 // Load config
 dotenv.config({ path: "./config/config.env" });
 
 connectDB();
 
 const app = express();
+
+//  Logging
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// Handlebars
+app.engine(".hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", ".hbs");
 
 const PORT = process.env.PORT || 3000;
 
