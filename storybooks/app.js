25d97664bfe979ee598fcb40b355ef1c36394356
vicdubbeld <vicdubbeld@gmail.com>
2020-11-09 18:11:36 -0800

    passport integration started

--- storybooks/app.js
+++ storybooks/app.js
@@ -3,10 +3,15 @@ const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const exphbs = require("express-handlebars");
+const passport = require("passport");
+const session = require("express-session");
 const connectDB = require("./config/db");
 
 // Load config
 dotenv.config({ path: "./config/config.env" });
+
+// Passport config
+require("./config/passport")(passport);
 
 connectDB();
 
@@ -20,6 +25,19 @@ if (process.env.NODE_ENV === "development") {
 // Handlebars
 app.engine(".hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", ".hbs");
+
+// Sessions
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false, //does not create session until something stored
+    cookie: { secure: true },
+  })
+);
+// Set Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Static folder
 app.use(express.static(path.join(__dirname, "public")));
--- storybooks/routes/index.js
+++ storybooks/routes/index.js
@@ -4,13 +4,15 @@ const router = express.Router();
 // @desc Login/Landing Page
 // @route GET /
 router.get("/", (req, res) => {
-  res.render("Login");
+  res.render('login' , {
+      layout: 'login',
+  })
 });
 
 // @desc Dashboard
 // @route GET /dashboard
 router.get("/dashboard", (req, res) => {
-  res.render("Dashboard");
+  res.render("dashboard");
 });
 
 module.exports = router;
